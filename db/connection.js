019915cbcd45d212c5f8d6763bb8b7f11643fd45
sdanimc <116037382+sdanimc@users.noreply.github.com>
2022-12-21 10:29:10 -0800

    updated db JS files to accomplish database retrieval and additions, inquirer prompts stil quit without allowing choice selection

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql =require('mysql2');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    //users need to change password to their personal mysql password
+    password: 'FisHiNg4b0Ot$',
+    database: 'company'
+},
+console.log('Connected to database')
+);
+db.connect(function (err) {
+    if (err) throw err;
+});
+module.exports = db;
--- db/index.js
+++ db/index.js
@@ -1,13 +1,37 @@
-const mysql =require('mysql2');
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    //users need to change password to their personal mysql password
-    password: 'FisHiNg4b0Ot$',
-    database: 'company'
+const db = require('./connection');
+class Database {
+    constructor(db){
+        this.db = db;
+    }
+    getAllEmployees(){
+        return this.db.promise().query(
+            'SELECT employees.id, employees.first, employees.last, roles.title, departments.name, roles.salary, CONCAT(manager.first, " ", manager.last) AS manager FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON role.department_id = department.id LEFT JOIN employees manager ON manager.id = employee.manager_id;'
+        )
+    }
+    getDepartments(){
+        return this.db.promise().query(
+            'SELECT departments.id, departments.name FROM department;'
+        )
+    }
+    getRoles(){
+        return this.db.promise().query(
+            'SELECT roles.id, roles.title, roles.salary, departments.name AS department FROM roles LEFT JOIN departments on roles.department_id = departments.id;'
+        )
+    }
+    addDepartment(newDepart){
+        return this.db.promise().query(
+            'INSERT INTO departments SET ?', newDepart
+        )
+    }
+    addEmployee(newEmploy){
+        return this.db.promise().query(
+            'INSERT INTO employees SET ?', newEmploy
+        )
+    }
+    addRole(newRole){
+        return this.db.promise().query(
+            'INSERT INTO roles SET ?', newRole
+        )
+    }
 }
-);
-db.connect(function (err) {
-    if (err) throw err;
-});
-module.exports = db;
+module.exports = new Database(db)
